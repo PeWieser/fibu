bc52f450dcb5289215b422cac57f2d2b1531abfc
MeLo <53431028+PeWieser@users.noreply.github.com>
2026-08-08 11:23:16 +0200

    fix: remove eslint-plugin-react-native-a11y, fix explicit-module-boundary-types typo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,5 @@
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
 import typescriptParser from '@typescript-eslint/parser';
-import reactNativeA11y from 'eslint-plugin-react-native-a11y';
 
 export default [
   {
@@ -15,17 +14,12 @@ export default [
     },
     plugins: {
       '@typescript-eslint': typescriptEslint,
-      'react-native-a11y': reactNativeA11y
     },
     rules: {
       '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
       '@typescript-eslint/explicit-module-boundary-types': 'off',
       '@typescript-eslint/no-explicit-any': 'error',
       'no-console': ['warn', { allow: ['warn', 'error'] }],
-      ...reactNativeA11y.configs.all.rules,
-      // We can disable some overly strict rules if necessary:
-      'react-native-a11y/has-valid-accessibility-ignores-invert-colors': 'off',
-      'react-native-a11y/has-accessibility-hint': 'off'
     }
   }
 ];

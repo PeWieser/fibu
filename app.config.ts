f8c8fd9c0f136c3f2e23845fcda3f02c9fbcb9d4
DJ-LeMo <semole@pm.me>
2026-08-08 09:38:36 +0200

    feat: update app configuration and modify start scripts for Android and iOS

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ import { ExpoConfig, ConfigContext } from 'expo/config';
 
 export default ({ config }: ConfigContext): ExpoConfig => ({
   ...config,
-  name: 'EchoVault',
-  slug: 'echovault',
+  name: 'Fibu',
+  slug: 'fibu',
   version: '1.0.0',
   orientation: 'portrait',
   icon: './assets/icon.png',
--- metro.config.js
+++ metro.config.js
@@ -2,5 +2,9 @@ const { getDefaultConfig } = require('expo/metro-config');
 const { withNativeWind } = require('nativewind/metro');
 
 const config = getDefaultConfig(__dirname);
+config.resolver.blockList = [
+  /.*\/node_modules\/\..*/,
+  /.*\\node_modules\\\..*/,
+];
 
 module.exports = withNativeWind(config, { input: './global.css' });
